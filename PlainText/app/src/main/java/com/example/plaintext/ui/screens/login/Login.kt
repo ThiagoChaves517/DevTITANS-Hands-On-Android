caa170fff82cd697ffa826720269c0937c48d3bd
Carlos Bruno <carlosbrunocb@gmail.com>
2025-09-13 13:59:59 -0400

    feat: Definição do layout do conteúdo da Tela Login
    Foi adicionado o layout da Tela de login criando-se a função LoginScreen
    composta de Composable separados. Os componentes de Tela Composable criados;
    CustomImageTextRow, LoginInput, CheckableRow e ButtonWithIcons,

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/login/Login.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/login/Login.kt
@@ -1,7 +1,11 @@
 package com.example.plaintext.ui.screens.login
 
+import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -10,6 +14,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,10 +23,15 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Login
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.VpnKey
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,9 +39,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
@@ -42,18 +54,25 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.plaintext.R
+import com.example.plaintext.ui.theme.PlainTextTheme
 import com.example.plaintext.ui.viewmodel.PreferencesViewModel
 
 data class LoginState(
@@ -98,7 +117,7 @@ fun MyAlertDialog(shouldShowDialog: MutableState<Boolean>) {
 @OptIn(ExperimentalMaterial3Api::class)
 fun TopBarComponent(
     navigateToSettings: (() -> Unit?)? = null,
-    navigateToSensores: (() -> Unit?)? = null,
+//    navigateToSensores: (() -> Unit?)? = null,
 ) {
     var expanded by remember { mutableStateOf(false) }
     val shouldShowDialog = remember { mutableStateOf(false) }
@@ -108,15 +127,21 @@ fun TopBarComponent(
     }
 
     TopAppBar(
-        title = { Text("PlainText") },
+        title = { Text("PlainText", color = colorResource(id = R.color.white)) },
         actions = {
-            if (navigateToSettings != null && navigateToSensores != null) {
+//            if (navigateToSettings != null && navigateToSensores != null) {
+            if (navigateToSettings != null) {
                 IconButton(onClick = { expanded = true }) {
-                    Icon(Icons.Default.MoreVert, contentDescription = "Menu")
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = "Menu",
+                        tint = colorResource(id = R.color.white)
+                    )
                 }
                 DropdownMenu(
                     expanded = expanded,
-                    onDismissRequest = { expanded = false }
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier.background(colorResource(id = R.color.black))
                 ) {
                     DropdownMenuItem(
                         text = { Text("Configurações") },
@@ -138,6 +163,290 @@ fun TopBarComponent(
                     )
                 }
             }
-        }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = colorResource(id = R.color.black)
+        )
     )
+}
+
+class Login : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PlainTextTheme {
+                Scaffold(
+                    topBar = {
+                        TopBarComponent()
+                    },
+                    containerColor = colorResource(id = R.color.black),
+                    contentColor = colorResource(id = R.color.white)
+                ) {
+                    // Modifier.padding(it) é usado para adicionar padding ao conteúdo da tela
+                    // para os casos em que a barra de navegação é exibida no Scaffold.
+                    // Isso é útil para evitar que o conteúdo seja sobreposto pela barra de navegação.
+                    LoginScreen(modifier = Modifier.padding(it))
+                }
+            }
+        }
+    }
+}
+
+// Composable que representa a pré-visualização da tela de login
+@Preview(name = "Modo Retrato", widthDp = 320)
+@Preview(name = "Modo Paisagem", widthDp = 640)
+@Composable
+fun PreviewUI() {
+    PlainTextTheme {
+        PlainTextTheme {
+            Scaffold (
+                topBar = {
+                    TopBarComponent()
+                },
+                containerColor = colorResource(id = R.color.black),
+                contentColor = colorResource(id = R.color.white)
+            ) {
+                LoginScreen(modifier = Modifier.padding(it))
+            }
+        }
+    }
+}
+
+@Composable
+// Tela de login
+private fun LoginScreen(modifier: Modifier){
+
+    val scrollState = rememberScrollState()
+
+    Column(
+        // Recebe as configurações de espaçamento para os elementos filhos da coluna.
+        // Neste caso informações de preenchimento (padding) da barra superior e inferior
+        // caso a barra de navegação esteja presente.
+        // fillMaxSize é usado para preencher o espaço disponível na tela.
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+            .padding(8.dp)
+    ){
+        var login by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var isChecked by remember { mutableStateOf(false) }
+        isChecked = false
+
+        CustomImageTextRow(
+            contentDescription = "cabeça de robô Android",
+            textResId = R.string.login_image_text
+        )
+
+        Text(
+            text = stringResource(id = R.string.login_title).uppercase(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 48.dp, bottom = 5.dp),
+            textAlign = TextAlign.Start,
+            fontWeight = FontWeight.Bold
+        )
+
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+        ){
+            LoginInput(
+                value = login,
+                onValueChange = { login = it },
+                label = stringResource(id = R.string.login_login),
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = Icons.Default.AccountBox)
+
+            LoginInput(
+                value = password,
+                onValueChange = { password = it },
+                label = stringResource(id = R.string.login_senha),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                leadingIcon = Icons.Default.VpnKey)
+        }
+
+        CheckableRow(
+            checked = isChecked,
+            onCheckedChange = { isChecked = it },
+            text = stringResource(id = R.string.login_isCheck),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            spacerWidth = 0.dp
+        )
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            horizontalArrangement  = Arrangement.Center
+        ){
+            ButtonWithIcons(
+                modifier = Modifier.fillMaxWidth(0.90f),
+                onClick = { /*TODO*/ },
+                text = stringResource(id = R.string.login_button),
+                leadingIcon = Icons.AutoMirrored.Filled.Login,
+                trailingIcon = Icons.AutoMirrored.Filled.Login,
+                trailingIconRotation = 180f
+            )
+        }
+    }
+}
+
+@Composable
+// Composable que representa uma linha com uma imagem e um texto
+fun CustomImageTextRow(
+    backgroundColor: Color = colorResource(id = R.color.login_row_background),
+    imageResId: Int = R.drawable.ic_launcher_foreground,
+    contentDescription: String,
+    textResId: Int,
+    imageStartPadding: Dp = 25.dp,
+    textImageSpacerWidth: Dp = 8.dp,
+    textEndPadding: Dp = 25.dp
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(backgroundColor),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = imageResId),
+            contentDescription = contentDescription,
+            modifier = Modifier.padding(start = imageStartPadding)
+        )
+
+        Spacer(Modifier.width(textImageSpacerWidth))
+
+        Text(
+            text = stringResource(id = textResId),
+            modifier = Modifier.padding(end = textEndPadding)
+        )
+
+        Spacer(
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+// Entradas de texto para o login e password
+fun LoginInput(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier,
+    leadingIcon: ImageVector = Icons.Default.AccountBox,
+    contentColor: Color = Color.White
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = label) },
+        leadingIcon = {
+            Icon(
+                imageVector = leadingIcon,
+                contentDescription = "Icone de Entradada de Texto"
+            )},
+        modifier = modifier,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedTextColor = contentColor,
+            unfocusedTextColor = contentColor,
+            focusedLeadingIconColor = contentColor,
+            unfocusedLeadingIconColor = contentColor,
+            focusedLabelColor = contentColor,
+            unfocusedLabelColor = contentColor,
+            focusedBorderColor = contentColor,
+            unfocusedBorderColor = contentColor
+        )
+    )
+}
+
+@Composable
+// Caixa de seleção de opção com texto
+fun CheckableRow(
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    text: String,
+    modifier: Modifier = Modifier,
+    spacerWidth: Dp = 5.dp,
+    contentColor: Color = Color.White
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+            colors = CheckboxDefaults.colors(
+                checkedColor = contentColor,
+                uncheckedColor = contentColor
+            )
+        )
+        Spacer(Modifier.width(spacerWidth))
+        Text(text = text)
+    }
+}
+
+@Composable
+// Botão com ícones à esquerda e à direita
+fun ButtonWithIcons(
+    onClick: () -> Unit,
+    text: String,
+    leadingIcon: ImageVector?,
+    trailingIcon: ImageVector?,
+    modifier: Modifier = Modifier,
+    leadingIconRotation: Float = 0f,
+    trailingIconRotation: Float = 0f,
+    spacerWidth: Dp = 8.dp,
+    buttonColor: Color = colorResource(id =  R.color.login_button)
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Ícone da esquerda
+            if (leadingIcon != null) {
+                Icon(
+                    imageVector = leadingIcon,
+                    contentDescription = "Ícone à esquerda do texto",
+                    modifier = Modifier.rotate(leadingIconRotation)
+                )
+            }
+
+            // Espaçamento e texto
+            if (leadingIcon != null && trailingIcon != null) {
+                Spacer(modifier = Modifier.width(spacerWidth))
+            }
+
+            Text(
+                text = text.uppercase(),
+                fontWeight = FontWeight.Bold
+            )
+
+            // Espaçamento e ícone da direita
+            if (leadingIcon != null && trailingIcon != null) {
+                Spacer(modifier = Modifier.width(spacerWidth))
+            }
+            if (trailingIcon != null) {
+                Icon(
+                    imageVector = trailingIcon,
+                    contentDescription = "Ícone à direita do texto",
+                    modifier = Modifier.rotate(trailingIconRotation)
+                )
+            }
+        }
+    }
 }
