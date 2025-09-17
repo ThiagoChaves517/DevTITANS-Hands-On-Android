e03eca8684f80263f06a17517de4442ba1c91c1c
ThiagoChaves517 <zuruarque@hotmail.com>
2025-09-16 22:40:04 -0400

    Basic layout created

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/editList/EditList.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/editList/EditList.kt
@@ -27,11 +27,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.plaintext.R
 import com.example.plaintext.data.model.PasswordInfo
 import com.example.plaintext.ui.screens.Screen
+import com.example.plaintext.ui.screens.login.CustomImageTextRow
 import com.example.plaintext.ui.screens.login.TopBarComponent
 
 data class EditListState(
@@ -46,6 +54,43 @@ fun isPasswordEmpty(password: PasswordInfo): Boolean {
 }
 
 @Composable
+// Composable que representa uma linha com uma imagem e um texto
+fun CustomImageTextRow(
+    backgroundColor: Color = colorResource(id = R.color.login_row_background),
+    imageResId: Int = R.drawable.ic_launcher_foreground,
+    contentDescription: String,
+    textResId: Int,
+    imageStartPadding: Dp = 25.dp,
+    textImageSpacerWidth: Dp = 8.dp,
+    textEndPadding: Dp = 25.dp
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(backgroundColor),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = imageResId),
+            contentDescription = contentDescription,
+            modifier = Modifier.padding(start = imageStartPadding)
+        )
+
+        Spacer(Modifier.width(textImageSpacerWidth))
+
+        Text(
+            text = stringResource(id = textResId),
+            modifier = Modifier.padding(end = textEndPadding)
+        )
+
+        Spacer(
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
 fun EditList(
     args: Screen.EditList,
     navigateBack: () -> Unit,
@@ -53,7 +98,6 @@ fun EditList(
 ) {
 
 }
-
 
 @Composable
 fun EditInput(
@@ -93,10 +137,58 @@ fun EditListPreview() {
         navigateBack = {},
         savePassword = {}
     )
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CustomImageTextRow(
+            contentDescription = "cabeça de robô Android",
+            textResId = R.string.login_image_text
+        )
+
+        Text(
+            text = stringResource(id = R.string.editlist_editing_password).uppercase(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 48.dp, bottom = 5.dp),
+            textAlign = TextAlign.Start,
+            fontWeight = FontWeight.Bold
+        )
+
+        EditInput("Nome")
+        EditInput("Usuário")
+        EditInput("Senha")
+        EditInput("Notas")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun EditInputPreview() {
-    EditInput("Nome")
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CustomImageTextRow(
+            contentDescription = "cabeça de robô Android",
+            textResId = R.string.login_image_text
+        )
+
+        Text(
+            text = stringResource(id = R.string.editlist_adding_new_password).uppercase(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 48.dp, bottom = 5.dp),
+            textAlign = TextAlign.Start,
+            fontWeight = FontWeight.Bold
+        )
+
+        EditInput("Nome")
+        EditInput("Usuário")
+        EditInput("Senha")
+        EditInput("Notas")
+    }
 }
